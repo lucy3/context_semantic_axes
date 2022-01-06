9f0f5f1e89269793c5fdce0fcdde0e59d714f086
Divya Tadimeti <dtadimeti@sequoia.ischool.berkeley.edu>
2022-01-06 06:28:04 +0000

    trying to fix merge

--- code/coref_reddit.py
+++ code/coref_reddit.py
@@ -13,9 +13,7 @@ import neuralcoref
 from collections import defaultdict
 from tabulate import tabulate
 
-# ROOT = '/global/scratch/lucy3_li/manosphere/'
-
-# TODO Read from Lucy's data, write to Divya's own log folder
+# Reading from Lucy's data, writing to Divya's log folder
 ROOT = '/mnt/data0/lucy/manosphere/'
 DLOGS = '/mnt/data0/dtadimeti/manosphere/logs/'
 POSTS = ROOT + 'data/submissions/'
@@ -23,24 +21,25 @@ LOGS = ROOT + 'logs/'
 COMMENTS = ROOT + 'data/comments/'
 ANN_FILE = ROOT + 'data/ann_sig_entities.csv'
 BOTS = LOGS + 'reddit_bots.txt'
-# SAMPLE_COMMENTS = '/mnt/data0/dtadimeti/manosphere/data/sample_data2'
 
 def main(): 
     '''
-    Output is formatted as 
-    subreddit \t cluster1word1$cluster1word2 \t cluster2word1$cluster2word2$cluster2word3$cluster2word4 \n
+    Output format: subreddit \t cluster1word1$cluster1word2 \t cluster2word1$cluster2word2$cluster2word3$cluster2word4 \n
     '''
-    count = 0
-    # load vocabulary 
-    words = []
+    # load vocabulary
     with open(ANN_FILE, 'r') as csvfile:
-        reader = csv.DictReader(csvfile)
+	reader = csv.DictReader(csvfile)
         for row in reader: 
             if row['keep'] == 'Y': 
-                words.append(row['entity'])
+                words_total.append(row['entity'])
+                
+    # remove 'she' and 'he' from vocab
+    words = words_total[:len(words_total) - 2]
+    print(words)
+
     
 
-    # load the coref thingy 
+    # load coref
     nlp = spacy.load('en')
     neuralcoref.add_to_pipe(nlp)
     
@@ -54,24 +53,9 @@ def main():
             bots.append(line)
             
       
-    denoms = dict()
-    nums = dict()  
-
-    for word in words:
-        denoms[word] = 0
-        nums[word] = [0, 0, 0]
-
-    
- 
-    list_A = ['she', 'her', 'hers']
-    list_B = ['he', 'him', 'his']
-    list_C = ['they', 'them', 'their', 'theirs']
-               
     outfile = open(DLOGS + 'coref_people/' + month, 'w')
-#     outfile = open(DLOGS + 'coref_people/' + "sample1", 'w')
     
     with open(COMMENTS + 'RC_' + month + '/part-00000', 'r') as infile: 
-#     with open(SAMPLE_COMMENTS, 'r') as infile: 
         for line in infile: 
             d = json.loads(line)
             text = d['body']
@@ -81,40 +65,17 @@ def main():
             doc = nlp(text)
             outstring = ""
             if text == "" or text == "[deleted]" or text == "[removed]":
-                count = count + 1
                 continue
+            
         
             span = doc[0 : len(doc)]
             head = span[0].head.text
-            root = span.root.text
-
-                          
+            root = span.root.text            
                       
-            for c in doc._.coref_clusters:
-                str_mentions = []
-                for token in c.mentions:
-                    str_mentions.append(token.text)
-                for word in words:
-                    if word in str_mentions:
-                        denoms[word] = denoms[word] + 1
-                        
-                        check_A = any(item in list_A for item in str_mentions)
-                        check_B = any(item in list_B for item in str_mentions)
-                        check_C = any(item in list_C for item in str_mentions)
-                
-                        if check_A:
-                            nums[word][0] += 1
-                        if check_B:
-                            nums[word][1] += 1
-                        if check_C: 
-                            nums[word][2] += 1
-                        
+            for c in doc._.coref_clusters:                        
            
                 # SCENARIO 1:
-                # if first index of span is determiner or possessive and head of first index in span is span.root
-                # remove the first index (which is the determiner or possessive)    
-                # and see if the rest of the span is in the vocab.
-                                
+         
                 if span[0].dep_ in {'det','poss'}:
                     if (head == root):
                         doc = doc[1:]
@@ -141,7 +102,6 @@ def main():
     else: 
         post_path = POSTS + 'RS_v2_' + month + '/part-00000'
     with open(post_path, 'r') as infile: 
-        count = 0
         for line in infile: 
             d = json.loads(line)
             text = d['selftext']
@@ -151,36 +111,17 @@ def main():
             doc = nlp(text)
             outstring = ""
             
-            
             if text == "" or text == "[deleted]" or text == "[removed]":
-                count = count + 1
                 continue
+            
+            # id_post:  2380578
             
             span = doc[0 : len(doc)]
             head = span[0].head.text
             root = span.root.text
                           
                       
-            for c in doc._.coref_clusters:
-                str_mentions = []
-                                        
-                for token in c.mentions:
-                    str_mentions.append(token.text)
-                for word in words:
-                    if word in str_mentions:
-                        denoms[word] = denoms[word] + 1 
-                        
-                        check_A = any(item in list_A for item in str_mentions)
-                        check_B = any(item in list_B for item in str_mentions)
-                        check_C = any(item in list_C for item in str_mentions)
-                
-                        if check_A:
-                            nums[word][0] += 1
-                        if check_B:
-                            nums[word][1] += 1
-                        if check_C: 
-                            nums[word][2] += 1
-                        
+            for c in doc._.coref_clusters:                        
                         
 #                 SCENARIO 1:           
                 if span[0].dep_ in {'det','poss'}:
@@ -205,26 +146,7 @@ def main():
                
     outfile.close()
     
-    
-    data = []
-    for word in words:
-        if denoms[word] != 0:
-            she = (nums[word][0])/(denoms[word])
-            he = (nums[word][1])/(denoms[word])
-            they = (nums[word][2])/(denoms[word])
-            data.append([word, she, he, they])
-                                
-                                
-    col_names = ["She", "He", "They"]
-                                
-    print(tabulate(data, headers=col_names, tablefmt="fancy_grid"))
-        
-    with open('table.txt', 'w') as f:
-        f.write(tabulate(data, headers=col_names, tablefmt="fancy_grid"))
-        
 
-
-
-
+       
 if __name__ == '__main__':
     main()
