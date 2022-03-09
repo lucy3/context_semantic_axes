086c5b7358adadd1297ad14d7847a9731e208ab3
Divya Tadimeti <dtadimeti@sequoia.ischool.berkeley.edu>
2022-03-09 05:16:49 +0000

    updating latest code

--- code/coref_forums.py
+++ code/coref_forums.py
@@ -2,7 +2,7 @@
 Goal: get pronouns that refer to
 a word in our vocabulary.
 '''
-import re
+
 import spacy
 import csv
 import json
@@ -13,82 +13,130 @@ import neuralcoref
 from collections import defaultdict
 
 
+# ROOT = '/global/scratch/users/dtadimeti/manosphere/'
+# POSTS = ROOT + 'data/submissions/'
+# LOGS = ROOT + 'logs/'
+# COMMENTS = ROOT + 'data/comments/'
+# ANN_FILE = ROOT + 'data/ann_sig_entities.csv'
+# BOTS = LOGS + 'reddit_bots.txt'
+
+
 ROOT = '/global/scratch/users/dtadimeti/manosphere/'
 LOGS = ROOT + 'logs/'
 FORUMS = ROOT + 'data/cleaned_forums/'
 ANN_FILE = ROOT + 'data/ann_sig_entities.csv'
 
+
+
+# ROOT = '/mnt/data0/lucy/manosphere/'
+# DLOGS = '/mnt/data0/dtadimeti/manosphere/logs/'
+# LOGS = ROOT + 'logs/'
+# POSTS = ROOT + 'data/submissions/'
+# COMMENTS = ROOT + 'data/comments/'
+# ANN_FILE = ROOT + 'data/ann_sig_entities.csv'
+# BOTS = DLOGS + 'reddit_bots.txt'
+
 def main():
     '''
-    Output is formatted as
-    subreddit \t cluster1_word1$cluster1_word2 \t cluster2_word1$cluster2_word2$cluster2_word3$cluster2_word4 \n
+    Output format: subreddit \t cluster1word1$cluster1word2 \t cluster2word1$cluster2word2$cluster2word3$cluster2word4 \n
     '''
     # load vocabulary
-    words_total = []
+    words = []
     with open(ANN_FILE, 'r') as csvfile:
         reader = csv.DictReader(csvfile)
         for row in reader:
             if row['keep'] == 'Y':
-                words_total.append(row['entity'])
+                # *CHANGED TO LOWERCASE*
+                words.append(row['entity'].lower())
 
-    # remove 'she' and 'he' from vocab
-    words = words_total[:len(words_total) - 2]
+    # *CHANGED HOW I REMOVE 'SHE' AND 'HE' FROM VOCAB*
+    pattern1 = "he"
+    pattern2 = "she"
 
-    # load the coref thingy
+    if pattern1 in words:
+        words.remove(pattern1)
+    if pattern2 in words:
+        words.remove(pattern2)
+
+
+    # load coref
     nlp = spacy.load('en')
     neuralcoref.add_to_pipe(nlp)
 
+    f = sys.argv[1]
     forum_name = sys.argv[1]
+    outfile = open(LOGS + 'coref_forums/' + forum_name, 'w')
 
-    outfile = open(LOGS + 'coref_people/' + forum_name, 'w')
+    error_outfile = open(LOGS + "forum_errors", 'w')
 
     with open(FORUMS + forum_name,'r') as infile:
         for line in infile:
             d = json.loads(line)
-            text = d['text_post']
-
-            if len(text) > nlp.max_length: continue 
-
-            if d['date_post'] is None: continue
-
-            if text == "" or text == "[deleted]" or text == "[removed]": continue
 
             # skip the really long post (id_post: 2380578)
             if forum_name == "incels" and d["id_post"] == 2380578: continue
 
+            text = d['text_post']
             date_post = d['date_post']
+            if d['date_post'] is None: continue
+
+            if not check_valid_forum(line):
+                outfile.write(date)
+                outfile.write("\n")
+                continue
+
             date = date_post[0:10]
-            doc = nlp(text)
-            outstring = ""
 
-            span = doc[0 : len(doc)]
-            head = span[0].head.text
-            root = span.root.text
+            try:
+                # run the coref on text
+                doc = nlp(text)
 
+            except MemoryError:
+                error_outfile.write(line + '\n')
+                continue
 
-            for c in doc._.coref_clusters:
-                if span[0].dep_ in {'det','poss'}:
-                    if (head == root):
-                        doc = doc[1:]
-                        cluster_contains_vocab = any(word.text in words for word in c.mentions)
-                        if cluster_contains_vocab:
-                            for s in c.mentions:
-                                outstring += s.text.lower() + "$"
+            else:
+                outstring = ''
+                for c in doc._.coref_clusters: # for coref cluster in doc
+                    keep_cluster = False
+                    for s in c.mentions: # for span in cluster
+                        if s.text.lower() in words: # SCENARIO 2
+                            keep_cluster = True
+                            break
+                        if s[0].dep_ in {'det','poss'}: # SCENARIO 1
+                            new_s = s[1:]
+                            if new_s.text.lower() in words:
+                                keep_cluster = True
+                                break
+                    if keep_cluster:
+                        curr_cluster = []
+                        for s in c.mentions: # for span in cluster
+                            entity = s.text.lower()
+                            entity = entity.replace("\n", "")
+                            curr_cluster.append(entity)
+                        outstring += "$".join(curr_cluster) + "\t"
 
-                else:
-                    cluster_contains_vocab = any(word.text in words for word in c.mentions)
-                    if cluster_contains_vocab:
-                        for s in c.mentions:
-                            outstring += s.text.lower() + "$"
-                        outstring += "\t"
-
-            outfile.write(date + "\t" + outstring)
-            outfile.write("\n")
+                outfile.write(date + "\t" + outstring)
+                outfile.write("\n")
 
 
     outfile.close()
 
 
+def check_valid_forum(line):
+    '''
+    For forums posts
+    '''
+    post = json.loads(line)
+    if 'text_post' not in post: return False
+    text = post['text_post']
+    if len(text) > 1000000: return False
+    if text == "" or text == "[deleted]" or text == "[removed]": return False
+
+    return True
+
+
 
 if __name__ == '__main__':
     main()
+
