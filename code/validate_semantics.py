ec669baab2b5b4deaad532de5538bf0b2af866cd
Lucy Li <lucy3_li@berkeley.edu>
2022-04-13 22:54:14 +0000

    moved some functions

--- code/validate_semantics.py
+++ code/validate_semantics.py
@@ -1,6 +1,6 @@
 '''
-Compare semantic differences between nouns.
-Filter each dataset for nouns that occur in WordNet. 
+This script does the following: 
+
 '''
 from collections import defaultdict
 import json
@@ -25,250 +25,6 @@ ROOT = '/mnt/data0/lucy/manosphere/'
 DATA = ROOT + 'data/'
 GLOVE = DATA + 'glove/'
 LOGS = ROOT + 'logs/'
-
-def nrc_vad():
-    '''
-    Input: NRC VAD file
-    Output: a json where keys are the different lexicons
-    and the inner dictionaries contain high and low value words 
-    '''
-    data_file = DATA + 'semantics/NRC-VAD-Lexicon-Aug2018Release/NRC-VAD-Lexicon.txt'
-    classes = {'valence' : {'high': [], 'low': []}, 
-               'arousal' : {'high': [], 'low': []}, 
-               'dominance' : {'high': [], 'low': []}, 
-                }
-    with open(data_file, 'r') as infile: 
-        for line in infile: 
-            contents = line.strip().split('\t')
-            word = contents[0]
-            syns = wn.synsets(word)
-            if 'n' not in set([x.pos() for x in syns]): continue
-            val = float(contents[1])
-            if val > 0.75: 
-                classes['valence']['high'].append(word)
-            elif val < 0.25: 
-                classes['valence']['low'].append(word)
-            aro = float(contents[2])
-            if aro > 0.75: 
-                classes['arousal']['high'].append(word)
-            elif aro < 0.25: 
-                classes['arousal']['low'].append(word)
-            dom = float(contents[3])
-            if dom > 0.75: 
-                classes['dominance']['high'].append(word)
-            elif dom < 0.25: 
-                classes['dominance']['low'].append(word)
-    with open(DATA + 'semantics/cleaned/nrc_vad.json', 'w') as outfile:
-        json.dump(classes, outfile)
-        
-def occupations(): 
-    '''
-    Occupations from wikipedia and labour bureau 
-    '''
-    classes = defaultdict(dict)
-    categories = ['art', 'health', 'other', 'sports', 'stem']
-    
-    curr_cat = None
-    for clss in categories: 
-        with open(DATA + 'semantics/Occupations_' + clss + '.csv', 'r') as infile: 
-            for line in infile: 
-                if line.startswith('#http') or line.startswith('#Note:'): continue
-                if line.startswith('#'): 
-                    curr_cat = line.strip().replace('#', '')
-                    classes[curr_cat]['high'] = []
-                    classes[curr_cat]['low'] = []
-    for clss in categories: 
-        with open(DATA + 'semantics/Occupations_' + clss + '.csv', 'r') as infile: 
-            for line in infile: 
-                if line.startswith('#http') or line.startswith('#Note:'): continue
-                if line.startswith('#'): 
-                    curr_cat = line.strip().replace('#', '')
-                    continue
-                job = line.strip()
-                clean_job = re.sub("[\(].*?[\)]", "", job)
-                if len(clean_job.split()) >= 3: continue
-                for clss in classes: 
-                    if clss == curr_cat: 
-                        classes[clss]['high'].append(job)
-                    else: 
-                        classes[clss]['low'].append(job)
-                        
-    for clss in classes: 
-        # remove duplicates
-        classes[clss]['high'] = list(set(classes[clss]['high']))
-        print(clss.upper())
-        print(classes[clss]['high'])
-        classes[clss]['low'] = list(set(classes[clss]['low']))
-    
-    # This version keeps wikipedia job formatting
-    with open(DATA + 'semantics/cleaned/occupations.json', 'w') as outfile:
-        json.dump(classes, outfile)
-        
-def get_occupation_pages_part1(): 
-    '''
-    For each occupation, get its wikipedia page and download wikitext
-    Some occupations do not have a wikipedia page, in which
-    case we leave them out. 
-    '''
-    glove_vocab = set()
-    with open(GLOVE + 'glove.6B.300d.txt', 'r') as infile:
-        for line in infile: 
-            contents = line.split()
-            glove_vocab.add(contents[0])
-            
-    with open(DATA + 'semantics/cleaned/occupations.json', 'r') as infile:
-        classes = json.load(infile)
-        
-    all_occs = set()
-    for clss in classes: 
-        all_occs.update(classes[clss]['high'])
-        all_occs.update(classes[clss]['low'])
-            
-    wiki_pages = {} # title : request response
-    for wiki_title in all_occs: 
-        toks = set(wiki_title.lower().split(' '))
-        if len(toks) > 2: 
-            # only unigrams and bigrams
-            continue
-        if toks & glove_vocab != toks: 
-            # tokens need to be in glove
-            continue
-        response = requests.get('https://en.wikipedia.org/w/api.php?action=parse&page=' + wiki_title + '&prop=wikitext&formatversion=2&format=json&redirects')
-        if not response.ok: 
-            print("Problem with", wiki_title)
-        response_dict = json.loads(response.text)
-        if 'error' in response_dict: 
-            print("Problem with", wiki_title, response_dict)
-        else: 
-            title = response_dict['parse']['title']
-            if 'List of' in title or 'Lists of' in title: 
-                continue
-            pageid = response_dict['parse']['pageid']
-            wiki_pages[wiki_title] = [title, pageid]
-    with open(DATA + 'semantics/occupation_wikipages.json', 'w') as outfile: 
-        json.dump(wiki_pages, outfile)         
-        
-    # This version removes wikipedia job formatting
-    for clss in classes: 
-        for g in classes[clss]: 
-            new_list = []
-            old_list = classes[clss][g]
-            for occ in old_list: 
-                new_occ = re.sub("[\(].*?[\)]", "", occ).lower()
-                new_list.append(new_occ)
-            classes[clss][g] = new_list
-    with open(DATA + 'semantics/cleaned/occupations.json', 'w') as outfile:
-        json.dump(classes, outfile)
-    
-def get_occupation_pages_part2(): 
-    '''
-    Use the page IDs from occupation_wikipages.json, 
-    grep for the file containing the cleaned wikitext, 
-    and then extracts the cleaned wikitext to a separate file. 
-    '''
-            
-    WIKI_TEXT = '/mnt/data0/corpora/wikipedia/text/'
-    
-    with open(DATA + 'semantics/occupation_wikipages.json', 'r') as infile: 
-        occ_pages = json.load(infile)
-    pages_occ = {}
-    for occ in occ_pages: 
-        pages_occ[str(occ_pages[occ][1])] = occ
-        
-    occ_sents = defaultdict(list)
-            
-    for folder in tqdm(os.listdir(WIKI_TEXT)): 
-        if folder == 'all_files.txt': continue
-        for f in os.listdir(WIKI_TEXT + folder): 
-            path = WIKI_TEXT + folder + '/' + f
-            with open(path, 'r') as infile: 
-                soup = BeautifulSoup(infile.read(), features="lxml")
-                docs = soup.find_all('doc')
-                for doc in docs: 
-                    idx = doc.get('id')
-                    if idx in pages_occ: 
-                        # found an occupation page
-                        occ = pages_occ[idx]
-                        occ = re.sub("[\(].*?[\)]", "", occ).lower()
-                        text = doc.get_text().split('\n')
-                        sents = []
-                        for l in text: 
-                            sents.extend(tokenize.sent_tokenize(l))
-                        for sent in sents: 
-                            sent_lower = sent.lower()
-                            matches = re.search(r'\b' + occ + r'\b', sent)
-                            if matches is not None: 
-                                occ_sents[occ].append(sent)
-    
-    with open(DATA + 'semantics/occupation_sents.json', 'w') as outfile: 
-        json.dump(occ_sents, outfile)
-    
-def prep_datasets():
-    #occupations()
-    #get_occupation_pages_part1()
-    get_occupation_pages_part2()
-    
-def get_semaxes(): 
-    '''
-    Default single word axes 
-    '''
-    axes_file = DATA + 'semantics/732_semaxis_axes.tsv'
-    axes = []
-    with open(axes_file, 'r') as infile: 
-        for line in infile: 
-            contents = line.strip().split('\t') 
-            axes.append(contents[0])
-            axes.append(contents[1])
-    return axes
-
-def retrieve_wordnet_axes(): 
-    '''
-    Uses the wider set of axes from WordNet, as seen
-    in the frameaxis paper. 
-    
-    Like in the semaxis paper where poles are expanded using
-    neighbors, here poles are expanded using synonyms
-    
-    Synsets are groups of synonymous words 
-    '''
-    glove_vocab = set()
-    with open(GLOVE + 'glove.6B.300d.txt', 'r') as infile:
-        for line in infile: 
-            contents = line.split()
-            glove_vocab.add(contents[0])
-            
-    i = 0
-    seen = set() # adjective clusters already seen
-    with open(LOGS + 'semantics_val/wordnet_axes.txt', 'w') as outfile: 
-        for ss in wn.all_synsets():
-            if ss.pos() == 'a': 
-                synonyms = set()
-                antonyms = set()
-                similar = ss.similar_tos() # similar synsets
-                for sim_ss in similar: 
-                    synonyms.update(sim_ss.lemma_names())
-                synonyms.update(ss.lemma_names())
-                for lem in ss.lemmas(): # lemmas in this synset             
-                    # get antonym lemmas and antonym's similar lemmas
-                    ants = lem.antonyms() # list of lemmas
-                    for ant in lem.antonyms(): 
-                        antonyms.update(ant.synset().lemma_names())
-                        for ant_sim_ss in ant.synset().similar_tos(): 
-                            antonyms.update(ant_sim_ss.lemma_names())
-                # check that word appears in GloVe
-                synonyms = synonyms & glove_vocab
-                antonyms = antonyms & glove_vocab
-                # remove '.' acronyms 
-                synonyms = [w for w in synonyms if '.' not in w]
-                antonyms = [w for w in antonyms if '.' not in w]
-                # check that pole is "robust"
-                if len(synonyms) < 3 or len(antonyms) < 3: continue
-                synonyms = ','.join(sorted(synonyms))
-                antonyms = ','.join(sorted(antonyms))
-                if synonyms in seen or antonyms in seen: continue
-                outfile.write(ss.name() + '\t' + synonyms + '\t' + antonyms + '\n')
-                seen.add(synonyms)
-                seen.add(antonyms)
                 
 def load_wordnet_axes(): 
     axes = {}
@@ -287,6 +43,7 @@ def load_wordnet_axes():
 
 def get_pole_matrix(glove_vecs, axes): 
     '''
+    Used by save_frameaxis_inputs(). 
     @input: 
     - glove vectors, wordnet axes
     @output: 
@@ -406,6 +163,10 @@ def save_frameaxis_inputs(file_path, sent_path, lexicon_name, exp_name):
     '''
     For a lexicon that has scores for different words,
     saves vectors associated with that lexicon.
+    @input: 
+        - lexicon_dict: output of occupations()
+        - sent_dict: dictonary of sentences containing occupations
+    @output: 
     '''
     with open(file_path, 'r') as infile:
         lexicon_dict = json.load(infile)
@@ -770,8 +531,6 @@ def inspect_axes(exp_name):
         loo_val_nonagg(in_folder, axes, exp_name)
     
 def main(): 
-#     prep_datasets()
-#     retrieve_wordnet_axes()
     #inspect_axes('default')
     #inspect_axes('glove-zscore')
 #     inspect_axes('bert-default')
